ab5dcc260837ac9fca2b05c41a1ee9a0c6b72793
jiangmingming <jiangmingming@senseauto.com>
2025-06-23 20:03:44 +0800

    L1-DeepSeek

---
+++ config/open_ai.py
@@ -0,0 +1,17 @@
+# Here you can deploy a local ai mode via ollama.
+# And then you can use the API to interact with the model via ollama server.
+# Here is the steps:
+# 1. install ollama
+# 2. run ollama serve as a service
+# 3. request the model via ollama server with style of open ai
+# Some references:
+# https://platform.openai.com/docs/guides/embeddings
+# https://ollama.ai/docs
+# https://github.com/jmorganca/ollama
+# https://github.com/jmorganca/ollama/blob/main/docs/api.md
+# https://github.com/jmorganca/ollama/blob/main/docs/models.md
+# https://github.com/jmorganca/ollama/blob/main/docs/config.md
+# https://github.com/jmorganca/ollama/blob
+
+BASE_URL="http://127.0.0.1:11434/v1"
+API_KEY="ollama"
---
+++ utils/open_ai.py
@@ -0,0 +1,38 @@
+import os
+import datetime
+
+import openai
+import tiktoken
+
+# from typing import List, Tuple
+from dotenv import load_dotenv, find_dotenv
+
+from pair_programming.config import open_ai as open_ai_config
+
+def NewOpenAIClient():
+    _ = load_dotenv(find_dotenv()) # read local .env file.
+    # openai.api_key = os.getenv("OPENAI_API_KEY")
+    client = openai.OpenAI(
+        base_url=open_ai_config.BASE_URL,
+        api_key=open_ai_config.API_KEY,  # this is also the default, it can be omitted
+        )
+    return client
+
+def get_base_url() -> str:
+    return open_ai_config.BASE_URL
+
+def get_api_key() -> str:
+    return open_ai_config.API_KEY
+
+def list_models(client: openai.OpenAI):
+    return client.models.list()
+
+def get_completion(client: openai.OpenAI, prompt: str, model: str):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0.0,
+    )
+
+    return response.choices[0].message.content
